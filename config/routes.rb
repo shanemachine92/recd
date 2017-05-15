4c5ed2566dedcdb5c711e242e101b623fe256e3a
shane zackery <shane@entelo.com>
2017-05-15 04:18:34 -0700

    configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
+  get 'about', to: 'pages#about'
 
   resources :recommendations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'pages#home'
 end
