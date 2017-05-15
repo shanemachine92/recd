d8c214f97d66960103c56eee39814235771b99e0
shane zackery <shane@entelo.com>
2017-05-15 05:28:20 -0700

    Update status for enum

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,3 @@
 class Review < ApplicationRecord
+  enum status: { draft: 0, published: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515115325) do
+ActiveRecord::Schema.define(version: 20170515122001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(version: 20170515115325) do
     t.string "fandom"
     t.string "slug"
     t.index ["slug"], name: "index_recommendations_on_slug", unique: true
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
+    t.string "submitted_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "users", force: :cascade do |t|
