d5982cf9767bb61fbdfd5e7518b7b34861ffba6f
shane zackery <shane@entelo.com>
2017-05-17 09:16:56 -0700

    create partial for review form and added body validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user, dependent: :destroy
   belongs_to :recommendation
+
+  validates :body, presence: true, length: { minimum: 5, maximum: 3000 }
 end
