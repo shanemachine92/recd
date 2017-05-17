9d1a78f4ff1f4bc4851c7b03022e4a620d0b28a8
shane zackery <shane@entelo.com>
2017-05-17 09:03:35 -0700

    Build review form on recs show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = current_user_reviews.build(review_params)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body)
+  end
 end
