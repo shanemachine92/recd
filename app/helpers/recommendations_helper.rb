49ffd80dccf6aca81199e9cce2219bfe995fd64b
shane zackery <shane@entelo.com>
2017-05-19 08:32:22 -0700

    update reviews footer styles

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,5 +1,5 @@
 module RecommendationsHelper
-  # def gravatar_helper user
-  #   image_tag "https://www.gravatar.com/avatar/#{Digest::MDS.hexdigest(user.email)}", width:40
-  # end
+  def gravatar_helper user
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
 end
