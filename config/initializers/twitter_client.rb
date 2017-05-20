ed9251e2af29d7223684c4840f3884fac876f4db
shane zackery <shane@entelo.com>
2017-05-20 11:58:31 -0700

    add twitter client and oauth for twitter

---
+++ config/initializers/twitter_client.rb
@@ -0,0 +1,8 @@
+require 'twitter'
+
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key =         ENV['LCuySYY2qdALv5IbZaCAs2qQ7']
+  config.consumer_secret =      ENV['r8TUgmbnJ7SlizG6dZuEfGqpKDSTkm7Bo2LsbqWJtRndE6rYTC']
+  config.access_token =         ENV['4775742853-5Yix32Sd2kBQRCnqriMWnXr59IBuE5PaOoPiZ9i']
+  config.access_token_secret =  ENV['WTiLREUgsGtbtJSHd02VK9qkUAL1IMncj7zB648k3tRU0']
+end
--- config/twitter.rb
+++
@@ -1,8 +0,0 @@
-require 'twitter'
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "LCuySYY2qdALv5IbZaCAs2qQ7"
-  config.consumer_secret     = "r8TUgmbnJ7SlizG6dZuEfGqpKDSTkm7Bo2LsbqWJtRndE6rYTC"
-  config.access_token        = "4775742853-5Yix32Sd2kBQRCnqriMWnXr59IBuE5PaOoPiZ9i"
-  config.access_token_secret = "WTiLREUgsGtbtJSHd02VK9qkUAL1IMncj7zB648k3tRU0"
-end
