a3d5f7dc21ffbbfedc6b43d6dfa9bae4f385a3a1
shane zackery <shane@entelo.com>
2017-05-20 10:13:57 -0700

    add keys for twitter api

---
+++ config/twitter_config.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key =   LCuySYY2qdALv5IbZaCAs2qQ7
+  config.consumer_secret = r8TUgmbnJ7SlizG6dZuEfGqpKDSTkm7Bo2LsbqWJtRndE6rYTC
+  config.oauth_token = 4775742853-5Yix32Sd2kBQRCnqriMWnXr59IBuE5PaOoPiZ9i
+  config.oauth_token_secret = WTiLREUgsGtbtJSHd02VK9qkUAL1IMncj7zB648k3tRU0
+end
