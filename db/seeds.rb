9d8e8c15d9564111cfb23535572319c08e27eee7
shane zackery <shane@entelo.com>
2017-05-23 14:31:22 -0700

    create new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,288 @@
-12.times do |rec|
-  Recommendation.create!(
-    title: "The Falling Leviathan",
-    url: "https://www.fanfiction.net/s/12209953/1/The-Falling-Leviathan",
-    author: "missminamurray969",
-    pairings: "Merrill/Female Hake",
-    summary: "Far away from Ferelden and the Blight, the Hawke family comes to Kirkwall, the city of chains. Things begin to go wrong, for everyone. 
-    The world is full of magic, after all, and nothing stays forgotten forever. Third in The Unwoven Tapestry series.",
-    rated: "T",
-    language: "English",
-    genre:"Adventure",
-    chapters: 32,
-    words: 115524,
-    rating: 3.7,
-    complete: true,
-    )
-end
+Recommendation.create!(
+  title: "The Falling Leviathan",
+  url: "https://www.fanfiction.net/s/12209953/1/The-Falling-Leviathan",
+  author: "missminamurray969",
+  pairings: "Merrill/Female Hake",
+  summary: "Far away from Ferelden and the Blight, the Hawke family comes to Kirkwall, the city of chains. Things begin to go wrong, for everyone. 
+  The world is full of magic, after all, and nothing stays forgotten forever. Third in The Unwoven Tapestry series.",
+  rated: "T",
+  language: "English",
+  genre:"Adventure",
+  chapters: 32,
+  words: 115524,
+  complete: true
+)
+
+Recommendation.create!(
+  title: "Siren",
+  url: "http://archiveofourown.org/works/10720869/chapters/23754468",
+  author: "Sapphire Smoke",
+  pairings: "Female Hawke/Isabela",
+  summary: "When something like a black cloud passed between her and them, she knew that it was either a whale swimming over her head, or a ship full of human beings, who never imagined that a pretty little mermaid was standing beneath them, holding out her white hands towards the keel of their ship.
+  -The Little Mermaid, Hans Christian Anderson, 1836",
+  rated: "T",
+  language: "English",
+  genre:"Friendship",
+  chapters: 3,
+  words: 5786,
+  complete: false
+)
+
+Recommendation.create!(
+  title: "Roots",
+  url: "https://www.fanfiction.net/s/10695587/1/Roots",
+  author: "missminamurray969",
+  pairings: "Female Hawke/Isabelae",
+  summary: "It's those blasted stones, the one's sitting at the bottom of her stomach every time Hawke looks at her, every time she feels herself losing her wings and growing roots in their stead. She's being weighed down, 
+  planting and intertwining with everything that this place has become, and Isabela fears that if she's not careful, soon she'll never be able to fly again",
+  rated: "M",
+  language: "English",
+  genre:"Romance",
+  chapters: 1,
+  words: 11542,
+  complete: true
+)
+
+Recommendation.create!(
+  title: "Together Be Left & Right Hands",
+  url: "https://archiveofourown.org/works/10529544",
+  author: "Starsmedic87",
+  pairings: "Leliana/Cassandra Pentaghast, Female Inquisitor/Josephine Montilyet",
+  summary: "No matter how hard they try, the right hand cannot be without the left, one always needs the other, bound as one by the Divine herself, 
+  the right and left hands must head for Kirkwall before war rages upon the Chantry.",
+  rated: "M",
+  language: "English",
+  genre:"Romance",
+  chapters: 12,
+  words: 43277,
+  complete: false
+)
+
+Recommendation.create!(
+  title: "Acts of the Divine",
+  url: "https://www.fanfiction.net/s/11291531/1/Acts-of-the-Divine",
+  author: "Axis II",
+  pairings: "Cassandra Pentaghast/Female Inquisitor, Isabela/Female Hake",
+  summary: "The soon-to-be-enthroned Divine Victoria summons her allies to Val Royeaux for a secret mission. The future of the Chantry rests in the
+  hands of three women spread across Thedas. A Tranquil mage, an Antivan noble and an elven thief; Sister Nightingale will need them all before she rises to the Sunburst Throne.",
+  rated: "M",
+  language: "English",
+  genre:"Humor",
+  chapters: 39,
+  words: 188225,
+  complete: true
+)
+
+Recommendation.create!(title: "Faith, Unshaken",
+  url: "https://archiveofourown.org/works/3601686",
+  author: "southwarden",
+  pairings: "Female Cousland/Leliana",
+  summary: "The temple has a way of reopening old wounds.",
+  rated: "T",
+  language: "English",
+  genre:"Angst",
+  chapters: 7,
+  words: 17277,
+  complete: true
+)
+
+Recommendation.create!(title: "Wicked Hearts",
+  url: "https://www.fanfiction.net/s/12209953/1/The-Falling-Leviathan",
+  author: "vorvol",
+  pairings: "Female Inquisitor/Sera, Female Adaar/Sera",
+  summary: "When one runs towards the light, one should never forget to check for shadows below. After all, even the sun goes down in paradise. \"Heh, she said \'goes down\'.\" 
+  (AKA: a rewriting of the Sera romance with a witty, sarcastic Adaar, because I think the romance could have been better.)",
+  rated: "T",
+  language: "English",
+  genre:"Adventure",
+  chapters: 48,
+  words: 108488,
+  complete: false
+)
+
+Recommendation.create!(title: "Heroes with Fading Titles",
+  url: "https://archiveofourown.org/works/7685773",
+  author: "Gamerqueer",
+  pairings: "Female Inquisitor/Josephine Montilyet, Leliana/Female Warden, Leliana/Josephine Montilyet, Female Inquisitor/Female Warden, Female Inquisitor/Leliana/Josephine Montilyet/Female Warden, 
+  Female Hawke/Isabela/Merrill, Female Warden/Merrill, Female Inquisitor/Leliana/Josephine Montilyet/Female Warden/Female Hawke/Isabela/Merrill, Dagna/Sera",
+  summary: "The Inquisition has been officially disbanded, and its heroes have to find their places in a changing world. Fortunately, they have each other.",
+  rated: "MA",
+  language: "English",
+  genre:"Romance",
+  chapters: 7,
+  words: 9809,
+  complete: false
+)
+
+Recommendation.create!(title: "A Curious Thing",
+  url: "http://archiveofourown.org/works/83725",
+  author: "Aziethe",
+  pairings: "Morrigan/Female Warden, Morrigan/Female Surana",
+  summary: "An exploration of the bittersweet romance arc between Morrigan and the female elf mage Warden, and the consequences of betraying a lover's trust.
+  Morrigan/f!Surana, femslash, rated for mature themes and violence.",
+  rated: "MA",
+  language: "English",
+  genre:"Romance",
+  chapters: 17,
+  words: 45534,
+  complete: true
+)
+
+Recommendation.create!(title: "The Wont of the Gods",
+  url: "https://www.fanfiction.net/s/11670747/1/The-Wont-of-the-Gods",
+  author: "MoniShea",
+  pairings: "Leliana/Female Cousland",
+  summary: "'We were a silent, hidden thought in the fields of oblivion, and we have become a voice that causes the heavens to tremble. 
+  Yesterday we were and today we have become, for this is the wont of the gods.' The year is 9:37 Dragon, shortly after the Kirkwall rebellion and the Hero of Ferelden is missing. With the help of Cassandra, Leliana is enlisted to track down her vanished lover.",
+  rated: "T",
+  language: "English",
+  genre:"Romance",
+  chapters: 7,
+  words: 42072,
+  complete: false
+)
+
+Recommendation.create!(title: "To Recollect",
+  url: "https://www.fanfiction.net/s/11568706/1/To-Recollect",
+  author: "Vigors",
+  pairings: "Morrigan/ Female Cousland",
+  summary: "A journey may end, but the memories always remain. Their views may differ, but the shared experiences and emotions remain in them both. 
+  Morrigan and her Warden walk different paths, but the fondness is still there along with events that brought them together.",
+  rated: "T",
+  language: "English",
+  genre:"Adventure",
+  chapters: 7,
+  words: 13155,
+  complete: false
+)
+
+Recommendation.create!(title: "A Rift Apart",
+  url: "https://archiveofourown.org/works/3859843",
+  author: "NZWW",
+  pairings: "Cassandra Pentaghast/Female Trevelyan, Female Inquisitor/Cassandra Pentaghast, Anders/Bethany Hawke, Female Hawke/Isabela, Blackwall/Josephine Montilyet",
+  summary: "Inquisitor Trevelyan and Cassandra Pentaghast become trapped in an underground dungeon after closing a rift in the Hinterlands.",
+  rated: "M",
+  language: "English",
+  genre:"Romance",
+  chapters: 50,
+  words: 143767,
+  complete: true
+)
+
+Recommendation.create!(title: "Do Not Fear the Dark",
+  url: "https://archiveofourown.org/works/5735701",
+  author: "Axis II",
+  pairings: "Female Inquisitor/Cassandra Pentaghast, Leliana/Female Warden, Female Hawke/Isabela",
+  summary: "A friend in danger causes an unlikely reunion at Skyhold and plunges the core of the Inquisition into the Deep Roads for secrets therein.",
+  rated: "M",
+  language: "English",
+  genre:"Adventure",
+  chapters: 24,
+  words: 101329,
+  complete: true
+)
+
+Recommendation.create!(title: "Paradise Circus",
+  url: "https://archiveofourown.org/works/3679014",
+  author: "betweenfactandbreakfast",
+  pairings: "Female Hawke/Isabela",
+  summary: "On the run from both Darkspawn and Templars, a sarcastic young apostate named Marian Hawke tries to make a life for herself and her family in Kirkwall. 
+  Her actions end up shaking the world in ways she could never have predicted. Set in a high-tech version of Thedas, this fic spans the entire plot of Dragon Age II.",
+  rated: "T",
+  language: "English",
+  genre:"Adventure",
+  chapters: 8,
+  words: 50497,
+  complete: false
+)
+
+Recommendation.create!(title: "The Winds of Fate",
+  url: "https://archiveofourown.org/works/573592",
+  author: "vanguardinitiate",
+  pairings: "Arbryna",
+  summary: "Marian Hawke's life could definitely be better. Flunking out of college, constantly fighting with her brother and disappointing her mother, and she's never had anything resembling a long-term relationship with a woman. 
+  That all changes, however, when a fight between her and her brother Carver sends them hurtling into a deep, dark pit--and into another world.
+
+  There's only one clue that could help them get home. A word, carved into the rock where they fell: Asha'bellanar. Now they have to find out what it means, and how it can help them get back to their own world.
+
+  Luckily, they're not alone. As fate would have it, they're discovered by a certain Rivaini pirate captain. 
+  Isabela's looking for a distraction, and helping the Hawke siblings get home will do the job just as well as anything else. Together with her crew of misfits, Isabela will help Marian and Carver find Asha'bellanar, and their way home.",
+  rated: "T",
+  language: "English",
+  genre:"Adventure",
+  chapters: 22,
+  words: 45470,
+  complete: true
+)
+
+Recommendation.create!(title: "Dreamers in the Fade",
+  url: "https://archiveofourown.org/works/7010761",
+  author: "citizenblue",
+  pairings: "Female Adaar/Cassandra Pentaghast, Female Inquisitor/Cassandra Pentaghast, Female Hawke/Isabela, Leliana/Female Mahariel, Leliana/Female Warden",
+  summary: "Three heroes and the women who loved them. A beginning, a middle, and an end. The Inquisitor, the Champion, and the Warden are different in many ways. The Warden fights because she must; it has become her reluctant duty.
+  The Champion fights in order to provide, and though she has failed in many ways, she still has those she continues to care for. The Inquisitor fights for the excitement. The adrenaline. She fights because this is indeed the greatest fight of her life. 
+  These three women are different in many ways, and they love in their own ways. The threat of an ancient monster looms, but each has their own story, and their words will not be drowned.",
+  rated: "T",
+  language: "English",
+  genre:"Romance",
+  chapters: 1,
+  words: 23956,
+  complete: true
+)
+
+Recommendation.create!(title: "Any Port in a Storm",
+  url: "http://archiveofourown.org/works/7995094/chapters/18300067",
+  author: "Imrryr",
+  pairings: "Female Hawke/Isabela/Merrill, Female Hawke/Isabela, Female Hawke/Merrill, Isabela/Merrill",
+  summary: "Marian Hawke, Isabela, Merrill, and an adventure on the high seas, featuring pirates, magical mirrors, demons, and Hawke’s butt as a major plot point. From a Dragon Age universe where the Hawke family never went to Kirkwall.",
+  rated: "T",
+  language: "English",
+  genre:"Adventure",
+  chapters: 2,
+  words: 21825,
+  complete: false
+)
+
+Recommendation.create!(title: "A Slash of Blue",
+  url: "https://archiveofourown.org/works/964977",
+  author: "todisturbtheuniverse",
+  pairings: "Female Hawke/Isabela, Bethany Hawke/Varric Tethras ",
+  summary: "Hawke has a small case of hero worship for Isabela: a series of ficlets spanning all three acts. (It might be a crush. Or a powerful dollop of lust. Nothing more than that, surely.)",
+  rated: "T",
+  language: "English",
+  genre:"Romance",
+  chapters: 50,
+  words: 21781,
+  complete: true
+)
+
+Recommendation.create!(title: "Across The Firelight",
+  url: "https://www.fanfiction.net/s/10872611/1/Across-The-Firelight",
+  author: "mEltas Aldaron 31",
+  pairings: "Morrigan/ Female Cousland",
+  summary: "Explores romance between F Cousland and Morrigan and all its complexities during DAOrigins. Amara Cousland is injured after the battle of Ostagar and is recovering in Flemeth's hut with Morrigan's help. 
+  Morrigan finds herself drawn closer to the gifted warden as they journey together to end the Blight. Amara's past is slowly revealed.",
+  rated: "M",
+  language: "English",
+  genre:"Romance",
+  chapters: 69,
+  words: 209324,
+  complete: true
+)
+
+Recommendation.create!(title: "Ostagar, ultima esperanza",
+  url: "hhttps://www.fanfiction.net/s/8181750/1/Ostagar-ultima-esperanza-Dragon-Age",
+  author: "missminamurray969",
+  pairings: "Merrill/Female Hake",
+  summary: "Far away from Ferelden and the Blight, the Hawke family comes to Kirkwall, the city of chains. Things begin to go wrong, for everyone. 
+  The world is full of magic, after all, and nothing stays forgotten forever. Third in The Unwoven Tapestry series.",
+  rated: "T",
+  language: "Español",
+  genre: "Adventure",
+  chapters: 9,
+  words: 14382,
+  complete: false
+)
 
 puts "20 recs created"
 
