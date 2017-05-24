ab2c6fab9a1947da61e5fcd9582dcd0f4d065c02
shane zackery <shane@entelo.com>
2017-05-24 09:47:58 -0700

    replace link to with button to in login helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
     if current_user.is_a?(GuestUser) 
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
+      (button_to "Login", new_user_session_path, class: style)
      else 
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+      button_to "Logout", destroy_user_session_path, method: :delete, class: style
     end 
   end
 
